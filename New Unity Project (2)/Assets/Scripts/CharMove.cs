7a3aabedce5a869869ca67023a4a70ebb2e54c2b
Unknown <217brandonsadler@gmail.com>
2018-09-12 09:26:36 -0600

    Added jump

--- New Unity Project (2)/Assets/Scripts/CharMove.cs
+++ New Unity Project (2)/Assets/Scripts/CharMove.cs
@@ -4,13 +4,41 @@ using UnityEngine;
 
 public class CharMove : MonoBehaviour {
 
+    //Player movement variables
+    public int MoveSpeed;
+    public float JumpHeight;
+
+    //Player ground controls
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask whatIsGround;
+    private bool grounded;
+
+
 	// Use this for initialization
 	void Start () {
 		
 	}
-	
+
+	void FixedUpdate()
+	{
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+	}
+
 	// Update is called once per frame
 	void Update () {
-		
+        if (Input.GetKeyDown (KeyCode.Space) && grounded) {
+            Jump();
+        }
 	}
+
+    //Function Jump
+    public void Jump() {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, JumpHeight);
+    }
+
+    public void Movement() {
+        
+    }
 }
+

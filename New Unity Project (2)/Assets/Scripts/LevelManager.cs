1ee24639726cdb770c44cb5eb3ba25532d287269
Unknown <217brandonsadler@gmail.com>
2018-10-02 10:28:32 -0600

    Added respawn

--- New Unity Project (2)/Assets/Scripts/LevelManager.cs
+++ New Unity Project (2)/Assets/Scripts/LevelManager.cs
@@ -37,22 +37,31 @@ public class LevelManager : MonoBehaviour {
     public IEnumerator RespawnPlayerCo(){
         //generate death penealty
         Instantiate(deathParticle, PC.transform.position, PC.transform.rotation); //The first thing is what we want to create (deathParticle), The second thing is where we want it to create it at (players position), Third is the rotation of the player.
+
         //Hide Player
-        PC.enabled = false;
+        //PC.enabled = false;
         PC.GetComponent<Renderer>().enabled = false;
 
         gravityStore = PC.GetComponent<Rigidbody2D>().gravityScale;
         PC.GetComponent<Rigidbody2D>().gravityScale = 0f;
         PC.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+
         //life penalty
         scoreManager.AddPoints(-lifePenaltyOnDeath);
+
         //point message
         Debug.Log("Player respawn");
+
         //respawn delay
         yield return new WaitForSeconds(respawnDelay);
+
         //Gravity restore
         PC.GetComponent<Rigidbody2D>().gravityScale = gravityStore;
+
         //Match Players transform position
         PC.transform.position = currentCheckPoint.transform.position;
+
+        Instantiate(respawnParticle, currentCheckPoint.transform.position, currentCheckPoint.transform.rotation);
+
     }
 }

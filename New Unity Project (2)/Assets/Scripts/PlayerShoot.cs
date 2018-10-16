7db7ef8ae78e72a427a33c515667dd747d8701c3
Unknown <217brandonsadler@gmail.com>
2018-10-16 10:28:10 -0600

    Fixed more bugs tried adding timer

--- New Unity Project (2)/Assets/Scripts/PlayerShoot.cs
+++ New Unity Project (2)/Assets/Scripts/PlayerShoot.cs
@@ -7,11 +7,12 @@ public class PlayerShoot : MonoBehaviour {
     public Transform firePoint;
     public GameObject projectile;
 
-
 	// Update is called once per frame
 	void Update () {
+
         if(Input.GetKeyDown(KeyCode.Mouse0)) {
             Instantiate(projectile, firePoint.position, firePoint.rotation);
         }
+
 	}
 }
--- New Unity Project (2)/Assets/Scripts/Projectile.cs
+++ New Unity Project (2)/Assets/Scripts/Projectile.cs
@@ -14,27 +14,38 @@ public class Projectile : MonoBehaviour {
 
     public int pointsForKill;
 
+    public float startTime = 0;
+
 	// Use this for initialization
 	void Start () {
-        PC = FindObjectOfType<Rigidbody2D>();
+        //PC = FindObjectOfType<Rigidbody2D>();
         if (PC.transform.localScale.x < 0) {
             speed = -speed;
         } //Ends if
+
 	} //Ends Start
 	
 	// Update is called once per frame
 	void Update () {
         GetComponent<Rigidbody2D>().velocity = new Vector2(speed, GetComponent<Rigidbody2D>().velocity.y);
-
+        startTime += Time.deltaTime;
 	} //Ends Update
 
-	void OnTriggerEnter2D(Collider2D other) {
+
+    void OnTriggerEnter2D(Collider2D other) {
+        float Time = startTime;
         if (other.tag == "Enemy"){
             Instantiate(enemyDeath, other.transform.position, other.transform.rotation);
             Destroy(other.gameObject);
             scoreManager.AddPoints(pointsForKill);
 
         } //Ends if
+        else if (Time >= 2)
+        {
+            Debug.Log("works.");
+            Destroy(gameObject);
+            startTime = 0;
+        }
         Instantiate(projectileParticle, transform.position, transform.rotation);
         Destroy(gameObject);
 

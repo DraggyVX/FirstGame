7621589a86d0f6e18b951668e6b5639bf31ed3ff
Unknown <217brandonsadler@gmail.com>
2018-10-22 10:08:45 -0600

    added another scene

---
+++ New Unity Project (2)/Assets/Scripts/NextLevel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextLevel : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.N)) {
+            SceneManager.LoadScene(sceneName: "Level2");
+        }
+	}
+}

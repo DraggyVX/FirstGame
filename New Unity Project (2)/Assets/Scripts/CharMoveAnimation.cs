f43944dc6154ba324faa7eef58109f68e1e8ea9b
Unknown <217brandonsadler@gmail.com>
2018-11-07 10:29:39 -0700

    Made scene good and added animation

---
+++ New Unity Project (2)/Assets/Scripts/CharMoveAnimation.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharMoveAnimation : MonoBehaviour {
+
+    public float period;
+    private float nextActionTime = 0;
+
+    public Sprite s1;
+    public Sprite s2;
+
+    private int spriteNumber;
+    private SpriteRenderer spriteRenderer;
+
+	// Use this for initialization
+	void Start () {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Time.time > nextActionTime) {
+            if (spriteNumber == 1){
+                spriteRenderer.sprite = s2;
+                spriteNumber = 2;
+            }
+            else {
+                spriteRenderer.sprite = s1;
+                spriteNumber = 1;
+            }
+            nextActionTime += period;
+        }
+		
+	}
+}

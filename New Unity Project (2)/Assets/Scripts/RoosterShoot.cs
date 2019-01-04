2ff6f593e34481408ae90755eee91c10547ea48d
Unknown <217brandonsadler@gmail.com>
2019-01-04 10:27:12 -0700

    Added stuff to third level

---
+++ New Unity Project (2)/Assets/Scripts/RoosterShoot.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoosterShoot : MonoBehaviour {
+
+    private int time;
+    private float timeout;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

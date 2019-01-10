63f9cfc099ca50f52954977fd1f809250a9df9d7
Unknown <217brandonsadler@gmail.com>
2019-01-10 10:04:31 -0700

    Finished game

---
+++ New Unity Project (2)/Assets/Scripts/LvL3.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LvL3 : MonoBehaviour {
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.layer != 0 && other.gameObject.layer != 11)
+            SceneManager.LoadScene("LvL3");
+
+    }
+}
